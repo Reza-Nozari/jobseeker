27c17dc9a22e26bd0070e3db9d6456080f5a3b86
Reza Nozari <dev.nozari@gmail.com>
2025-08-23 18:52:45 +0330

    implement first migration

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -4,7 +4,7 @@
  */
 
 import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core'; 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
